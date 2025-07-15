f1f176897063e532ec86bff2ffa0b96f0f822251
Wexos <wexosmk@gmail.com>
2025-07-15 16:53:04 +0200

    Small stuff

---
+++ Include/Game/CVector2f.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Types.h"
+
+class CVector2f
+{
+public:
+
+private:
+    f32 x;
+    f32 y;
+};
---
+++ Include/Game/CVector3f.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Types.h"
+
+class CVector3f
+{
+public:
+
+private:
+    f32 x;
+    f32 y;
+    f32 z;
+};
